910c9892bf9cf7e86eed960ffcbcf352af2a8c95
gu <19kim@aift.kr>
2024-07-09 09:01:25 +0000

    Update .gitignore and remove cached files

--- symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SRC_DIR="/usr/local/cuda/targets/x86_64-linux/lib"
-DEST_DIR="/opt/conda/lib/python3.10/site-packages/llama_cpp/lib"
-
-for file in $SRC_DIR/*; do
-    ln -s "$file" "$DEST_DIR/$(basename "$file")"
-done
